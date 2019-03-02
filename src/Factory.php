6e8a03fb5a50e414ba0d7b4cd58561f568362d9f
Mariano Custiel <jmcustiel@gmail.com>
2019-03-02 16:39:18 +0100

    Added factory

---
+++ src/Factory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of Phiremock.
+ *
+ * Phiremock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Phiremock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Phiremock.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Mcustiel\Phiremock;
+
+use Mcustiel\Phiremock\Common\Utils\ArrayToExpectationConverter;
+use Mcustiel\Phiremock\Common\Utils\ArrayToRequestConverter;
+use Mcustiel\Phiremock\Common\Utils\ArrayToResponseConverter;
+
+class Factory
+{
+    public function createArrayToExpectationConverter()
+    {
+        return new ArrayToExpectationConverter(
+            new ArrayToRequestConverter(),
+            new ArrayToResponseConverter()
+        );
+    }
+}
