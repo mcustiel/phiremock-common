b7156588f69b5598bb419a2b5b0fe9754d2651f5
Mariano Custiel <jmcustiel@gmail.com>
2020-11-16 17:28:34 +0100

    Added test for Contains matcher

---
+++ tests/unit/Domain/Condition/Matchers/ContainsTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Mcustiel\Phiremock\Tests\Unit\Domain\Condition\Matchers;
+
+use PHPUnit\Framework\TestCase;
+use Mcustiel\Phiremock\Domain\Condition\Matchers\Contains;
+use Mcustiel\Phiremock\Domain\Condition\StringValue;
+
+class ContainsTest extends TestCase
+{
+    /** @var Contains */
+    private $matcher;
+
+    protected function setUp(): void
+    {
+        $this->matcher = new Contains(new StringValue('Potato'));
+    }
+
+    public function testMatches(): void
+    {
+        $this->assertTrue($this->matcher->matches('Potato Tomato'));
+        $this->assertTrue($this->matcher->matches('Tomato Potato'));
+    }
+
+    public function testDoesNotMatch(): void
+    {
+        $this->assertFalse($this->matcher->matches('Coconut'));
+        $this->assertFalse($this->matcher->matches('Banana'));
+        $this->assertFalse($this->matcher->matches(' '));
+    }
+}
